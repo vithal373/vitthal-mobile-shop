b12e55db71fa5768f0579caf89bf55965fe097e6
vithal373 <omelectronics284@gmail.com>
2026-09-09 11:45:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,12 +8,12 @@
 // (येथे तुमचे स्वतःचे Firebase Project चे credentials पेस्ट करा)
 // -------------------------------------------------------------
 const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDLM3nuNhKKcOqtI2uMgcaJNPMVrfIlN48",
+  authDomain: "vitthal-mobile-shop.firebaseapp.com",
+  projectId: "vitthal-mobile-shop",
+  storageBucket: "vitthal-mobile-shop.firebasestorage.app",
+  messagingSenderId: "751867746433",
+  appId: "1:751867746433:web:8947a9df55877d2919d580"
 };
 
 // Global Store State
